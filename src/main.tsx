fc4fa89f4793738de5d04521b66c9948f33f25b0
Horikaze <adrianyt659@gmail.com>
2023-11-01 19:21:52 +0100

    added star background

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import StarsCanvas from "./components/main/StarBackground.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <StarsCanvas />
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
