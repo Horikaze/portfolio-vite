4264c133cb9c85a931b05834c94dfe5831673141
Horikaze <adrianyt659@gmail.com>
2023-11-01 18:21:09 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Hero from "./components/main/Hero";
+
+function App() {
+  return (
+    <main className="h-full w-full">
+      <div className="flex h-[850px] flex-col gap-20">
+        <Hero />
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/components/sub/HeroContent.tsx
@@ -0,0 +1,67 @@
+import { motion } from "framer-motion";
+import {
+  slideInFromLeft,
+  slideInFromRight,
+  slideInFromTop,
+} from "../../utils/motion";
+import { SparklesIcon } from "@heroicons/react/20/solid";
+export default function HeroContent() {
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
+        <motion.div
+          variants={slideInFromTop}
+          className="Welcome-box py-[8px] px-[4px] border border-[#7042f88b] opacity-[0.9]"
+        >
+          <SparklesIcon className="text-[#b49bff] mr-[10px] w-5 h-5" />
+          <h1 className="Welcome-text text-[13px]">
+            Fullstack Developer Portfolio
+          </h1>
+        </motion.div>
+        <motion.div
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+          variants={slideInFromLeft(0.5)}
+        >
+          Providing{" "}
+          <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
+            the best
+          </span>
+          project experience
+        </motion.div>
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas
+          voluptatem similique adipisci nihil quo commodi saepe vero accusamus
+          quia ea eaque enim cupiditate ad consequuntur non id, modi, ullam
+          sapiente sint maiores pariatur? Autem nobis debitis repudiandae ut
+          tempore temporibus laudantium necessitatibus rem ullam enim, corporis
+          minima tempora sit quas minus ad illum sunt consequuntur. Distinctio
+          facere accusantium veritatis iste.
+        </motion.p>
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+        <motion.div
+          variants={slideInFromRight(0.8)}
+          className="w-full h-full flex justify-center items-center"
+        >
+          <img
+            src="/mainIconsdark.svg"
+            alt="work icons"
+            height={650}
+            width={650}
+          />
+        </motion.div>
+      </div>
+    </motion.div>
+  );
+}
